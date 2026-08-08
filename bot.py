ced3925254df16d88dc44f96ccc9dfefa62fde7a
Yutabek <otabekrahmonov172@gmail.com>
2026-08-09 03:15:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,17 @@
 import asyncio
-import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from dotenv import load_dotenv
 
+from config import BOT_TOKEN
+from handlers.start import router as start_router
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv(8316292761:AAET6f_wIs4aEoBCuPVkA20san0JtA68KGQ)
 
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer(
-        "🎭 Xush kelibsiz!\n\n"
-        "Guruhimizga qo‘shilganingizdan xursandmiz.\n\n"
-        "O‘yinda qatnashishdan oldin guruh qoidalari "
-        "bilan tanishib chiqing."
-    )
+dp.include_router(start_router)
 
 
 async def main():
-    if not BOT_TOKEN:
-        raise ValueError("BOT_TOKEN topilmadi!")
-
     bot = Bot(token=BOT_TOKEN)
 
     print("Bot ishga tushdi...")
