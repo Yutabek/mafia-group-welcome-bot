09a170d18f96af294f3f2d7d0b49650e9cbfc095
Yutabek <otabekrahmonov172@gmail.com>
2026-08-09 03:16:27 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 router = Router()
@@ -8,9 +8,23 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="📖 Qoidalar bilan tanishish",
+                    callback_data="rules"
+                )
+            ]
+        ]
+    )
+
     await message.answer(
-        "🎭 Xush kelibsiz!\n\n"
+        "🎭 <b>Xush kelibsiz!</b>\n\n"
         "Guruhimizga qo‘shilganingizdan xursandmiz.\n\n"
         "O‘yinda qatnashishdan oldin guruh qoidalari "
-        "bilan tanishib chiqing."
+        "bilan tanishib chiqing.",
+        reply_markup=keyboard,
+        parse_mode="HTML"
     )
