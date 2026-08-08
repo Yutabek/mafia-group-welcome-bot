7c40342b3bea5c931d78e069eefba4c44250ec36
Yutabek <otabekrahmonov172@gmail.com>
2026-08-09 03:19:14 +0500

    Add BOT_USERNAME environment variable check

--- config.py
+++ config.py
@@ -7,6 +7,11 @@ load_dotenv()
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN .env faylida topilmadi!")
+
+if not BOT_USERNAME:
+    raise ValueError("BOT_USERNAME .env faylida topilmadi!")
